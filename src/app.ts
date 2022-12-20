8b8dee1260c24500c6418bf441bbe1039fec72f0
Eiji Tachikawa <tsuyoku1202@gmail.com>
2022-12-20 19:29:27 +0900

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+// デコレータは関数で、特定の方法でクラスで使用する
+// 慣習的に一文字目は大文字で
+// 引数にはデコレータの対象となるものを
+// デコレータはクラスが定義されたときに実行される（インスタンス化よりも先）
+function Logger(constructor: Function) {
+  console.log("ログ出力中...");
+  console.log(constructor);
+}
+
+// デコレータとして使う関数名を記述
+@Logger
+class Person {
+  name = "Max";
+
+  constructor() {
+    console.log("Personオブジェクトを作成中...")
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
